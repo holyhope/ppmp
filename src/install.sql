8b824f9da365802509a167741c0c42ed2b033924
Maxime Pichou <Maxime>
2015-12-16 15:58:36 +0100

    Add Jackson Library Modify install.sql

--- src/install.sql
+++ src/install.sql
@@ -1,11 +1,19 @@
-create database online_cv;
-
-CREATE TABLE IF NOT EXISTS users
+create database IF NOT EXISTS online_cv;
+ALTER DATABASE online_cv CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+CREATE TABLE IF NOT EXISTS users(
     id INT(11) PRIMARY KEY AUTO_INCREMENT NOT NULL,
     last_name VARCHAR(255) NULL,
     first_name VARCHAR(255) NULL,
     birthdate DATE NULL,
     email VARCHAR(255) NOT NULL,
-    photo_path VARCHAR(255) NULL;
-    
-    
+    photo_path VARCHAR(255) NULL
+    )ENGINE=InnoDB CHARACTER SET=utf8;
+
+CREATE TABLE IF NOT EXISTS phone(
+	id INT(11) NOT NULL,
+	number VARCHAR(15) NULL,
+	type VARCHAR(5) NULL,
+	id_users INT(11),
+	PRIMARY KEY(id),
+	FOREIGN KEY (id_users) REFERENCES users(id)
+	)ENGINE=InnoDB CHARACTER SET=utf8;    
