8faa54a74b82dbb665ee73de48875916fd5cebf2
Pierre Péronnet <pierre.peronnet@gmail.com>
2016-02-08 12:26:04 +0100

    Add isConnected. Add comments.

--- src/fr/onlineCV/beans/SessionBean.java
+++ src/fr/onlineCV/beans/SessionBean.java
@@ -6,12 +6,41 @@ import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpSession;
 
 public class SessionBean {
-	public static HttpSession getSession() {
+
+	/**
+	 * Get session of the current visitor.
+	 * 
+	 * @return Session of the visitor.
+	 * @throws NullPointerException
+	 *             if visitor is not connected.
+	 */
+	public static HttpSession getSession() throws NullPointerException {
 		return (HttpSession) Objects
 				.requireNonNull(FacesContext.getCurrentInstance().getExternalContext().getSession(false));
 	}
 
+	/**
+	 * Create and get a session for the current visitor. If you only wants to
+	 * get the current session, use SessionBean.getSession().
+	 * 
+	 * @see SessionBean.getSession
+	 * @return new session for the visitor.
+	 */
 	public static HttpSession createSession() {
 		return (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(true);
+	}
+
+	/**
+	 * Check if the current visitor is connected.
+	 * 
+	 * @return True if the visitor is connected.
+	 */
+	public static boolean isConnected() {
+		try {
+			getSession();
+			return true;
+		} catch (NullPointerException e) {
+			return false;
+		}
 	}
 }
