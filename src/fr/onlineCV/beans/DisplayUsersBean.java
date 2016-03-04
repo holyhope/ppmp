d99aab0f277c7808f46f04e1267edb5679541f59
Maxime Pichou <maximepichou@users.noreply.github.com>
2016-03-04 16:35:10 +0100

    Add boolean value to display remove button with edit button

--- src/fr/onlineCV/beans/DisplayUsersBean.java
+++ src/fr/onlineCV/beans/DisplayUsersBean.java
@@ -15,6 +15,10 @@ public class DisplayUsersBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	private User user;
+	private boolean showDeleteButtonSkill = false;
+	private boolean showDeleteButtonHobby = false;
+	private boolean showDeleteButtonExperiences = false;
+	private boolean showDeleteButtonDiploma = false;
 	
 	@EJB
 	private UsersDAO userDao;
@@ -23,6 +27,48 @@ public class DisplayUsersBean implements Serializable {
 		user = (User) SessionBean.getSession().getAttribute(LoginBean.USER);
 		user = userDao.find(user.getEmail());
 	}
+	
+	public void modifySkill(){
+		if(showDeleteButtonSkill==true){
+			showDeleteButtonSkill=false ;
+		}
+		else{
+			showDeleteButtonSkill = true;
+		}
+		
+	}
+	public void modifyHobby(){
+		if(showDeleteButtonHobby==true){
+			showDeleteButtonHobby=false ;
+		}
+		else{
+			showDeleteButtonHobby = true;
+		}
+		
+	}
+	public void modifyExperiences(){
+		if(showDeleteButtonExperiences==true){
+			showDeleteButtonExperiences=false ;
+		}
+		else{
+			showDeleteButtonExperiences = true;
+		}
+		
+	}
+	public void modifyDiploma(){
+		if(showDeleteButtonDiploma==true){
+			showDeleteButtonDiploma=false ;
+		}
+		else{
+			showDeleteButtonDiploma = true;
+		}
+		
+	}
+	
+	
+	public void saveChanges(){
+		userDao.update(user);
+	}
 
 	public User getUser() {
 		return user;
@@ -30,6 +76,38 @@ public class DisplayUsersBean implements Serializable {
 
 	public void setUser(User user) {
 		this.user = user;
+	}
+
+	public boolean isShowDeleteButton() {
+		return showDeleteButtonSkill;
+	}
+
+	public void setShowDeleteButton(boolean showDeleteButton) {
+		this.showDeleteButtonSkill = showDeleteButton;
+	}
+
+	public boolean isShowDeleteButtonHobby() {
+		return showDeleteButtonHobby;
+	}
+
+	public void setShowDeleteButtonHobby(boolean showDeleteButtonHobby) {
+		this.showDeleteButtonHobby = showDeleteButtonHobby;
+	}
+
+	public boolean isShowDeleteButtonExperiences() {
+		return showDeleteButtonExperiences;
+	}
+
+	public void setShowDeleteButtonExperiences(boolean showDeleteButtonExperiences) {
+		this.showDeleteButtonExperiences = showDeleteButtonExperiences;
+	}
+
+	public boolean isShowDeleteButtonDiploma() {
+		return showDeleteButtonDiploma;
+	}
+
+	public void setShowDeleteButtonDiploma(boolean showDeleteButtonDiploma) {
+		this.showDeleteButtonDiploma = showDeleteButtonDiploma;
 	}
 
 	
